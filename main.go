75de30615226b0bae45e8b2bf2f6770b505fb506
pablo ramirez <pablodario2014@gmail.com>
2021-03-05 12:12:12 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+/*
+package main
+
+import ("fmt")
+
+  func main(){
+
+    fmt.Println("Hola como va")
+
+}
+*/
+
+//Variables
+//Para declarar variables
+//Go es un lneguaje fuertemente tipeado
+//Lo ideal es que las variables no cambien
+
+//La palabra para declarar varibles es variables
+//Todas las variables tienen un valor inicial
+//No se pueden dejar variables sin utilizar
+
+package main
+import "fmt"
+
+func main(){
+ 
+  nombre := "Pablo"
+    fmt.Println(nombre) 
+
+  	edad := 23
+    fmt.Println(edad)
+}
